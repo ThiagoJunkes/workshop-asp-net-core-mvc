4d133931eb4108e78f11c55fb48bbd23a985e6d1
ThiagoJunkes <thiago.junkes3@gmail.com>
2023-06-28 10:50:33 -0300

    MySQL adpatation and first migration

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -1,9 +1,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using MySqlConnector;
 using SalesWebMvc.Data;
+using System;
+using System.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<SalesWebMvcContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("SalesWebMvcContext") ?? throw new InvalidOperationException("Connection string 'SalesWebMvcContext' not found.")));
+
+string mySqlConnection = builder.Configuration.GetConnectionString("SalesWebMvcContext");
+builder.Services.AddDbContextPool<SalesWebMvcContext>(options =>
+options.UseMySql(mySqlConnection, ServerVersion.AutoDetect(mySqlConnection), builder => builder.MigrationsAssembly("SalesWebMvc")));
+
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -17,6 +25,10 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+//builder.Services.AddDbContext<SalesWebMvcContext>(options =>
+//options.UseMySql(builder.Configuration.GetConnectionString("SalesWebMvcContext"), MySqlServerVersion.LatestSupportedServerVersion, builder => builder.MigrationsAssembly("SalesWebMvc")));
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
