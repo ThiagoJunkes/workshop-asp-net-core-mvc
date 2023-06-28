60dbbb7acccf347dbb9d18c4323ea2d1f14685d2
ThiagoJunkes <thiago.junkes3@gmail.com>
2023-06-28 15:24:27 -0300

    SellerFormViewModel and Department select component

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -19,6 +19,7 @@ options.UseMySql(mySqlConnection, ServerVersion.AutoDetect(mySqlConnection), bui
 builder.Services.AddControllersWithViews();
 builder.Services.AddScoped<SeedingService>();
 builder.Services.AddScoped<SellerService>();
+builder.Services.AddScoped<DepartmentService>();
 
 var app = builder.Build();
 
