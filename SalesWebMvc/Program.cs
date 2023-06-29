b767c87e5d3905a4eb0a9c38e780a0123edfd4aa
ThiagoJunkes <thiago.junkes3@gmail.com>
2023-06-29 10:01:42 -0300

    App locale, number and date formatting

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -1,12 +1,14 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Localization;
 using MySqlConnector;
 using SalesWebMvc.Data;
 using SalesWebMvc.Models;
 using SalesWebMvc.Services;
 using System;
 using System.Configuration;
+using System.Globalization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +16,13 @@ string mySqlConnection = builder.Configuration.GetConnectionString("SalesWebMvcC
 builder.Services.AddDbContextPool<SalesWebMvcContext>(options =>
 options.UseMySql(mySqlConnection, ServerVersion.AutoDetect(mySqlConnection), builder => builder.MigrationsAssembly("SalesWebMvc")));
 
+var enUS = new CultureInfo("en-US");
+var localizationOptions = new RequestLocalizationOptions
+{
+    DefaultRequestCulture = new RequestCulture(enUS),
+    SupportedCultures = new List<CultureInfo> { enUS },
+    SupportedUICultures = new List<CultureInfo> { enUS }
+};
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -44,7 +53,7 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-
+app.UseRequestLocalization(localizationOptions);
 app.Services.CreateScope().ServiceProvider.GetRequiredService<SeedingService>().Seed();
 
 
